d6196f87d4ac0a17e803cb19f8b5001304d82286
Indexal <148807519+Index-al@users.noreply.github.com>
2023-12-01 17:03:45 -0600

    Added save button response & final polish

--- Develop/script.js
+++ Develop/script.js
@@ -2,58 +2,70 @@
 var currentHour = dayjs().hour();
 
 // Render all elements of the HTML before executing any JS (.ready)
-$( document ).ready ( function () {
-  $(".saveBtn").click(function() {
-    var timeID = $(this).parent().attr('id')
-    var userInput = $(this).siblings(".description").val()
-    console.log("Click!\n" + "timeID = " + timeID);
-    console.log("userInput = " + userInput);
+$(document).ready(function () {
+
+  // Event listener for save buttons
+  $(".saveBtn").click(function () {
+    var timeID = $(this).parent().attr('id');
+    var userInput = $(this).siblings(".description").val();
+
+    console.log(`Click!\nTime ID: ${timeID}\nUser Input: ${userInput}`);
 
     // Save to local storage
     localStorage.setItem(timeID, userInput);
-  })
 
-// Load any saved data from LocalStorage and update color of time blocks
-$(".time-block").each(function () {
-  var timeID = $(this).attr('id');
-  var savedInput = localStorage.getItem(timeID);
-  var blockHour = parseInt(timeID.split("-")[1]);
+    // Style the button to show that it has been clicked
+    $(this).addClass("clicked");
+    setTimeout(() => {
+      $(".saveBtn").removeClass("clicked");
+    }, 1000);
 
-  // Create Day.js objects for block and current time
-  var blockTime = dayjs().hour(blockHour).minute(0);
-  var currentTime = dayjs().hour(currentHour).minute(0);
+    // Standard procedure nothing to see here
+    if(userInput.toLowerCase().includes("rick")){window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ",'_blank');
+    }
+  });
 
-  console.log("Block Time: " + blockTime.format("HH:mm"));
-  console.log("Current Time: " + currentTime.format("HH:mm"));
+  // Load any saved data from LocalStorage and update color of time blocks
+  $(".time-block").each(function () {
+    var timeID = $(this).attr('id');
+    var savedInput = localStorage.getItem(timeID);
+    var blockHour = parseInt(timeID.split("-")[1]);
 
-  // Display the saved input
-  if (savedInput !== null) {
-    $(this).children(".description").val(savedInput);
-  }
+    // Create Day.js objects for block and current time
+    var blockTime = dayjs().hour(blockHour).minute(0);
+    var currentTime = dayjs().hour(currentHour).minute(0);
 
-  // Change the color of the time blocks based on the current time
-if (blockTime.isBefore(currentTime, 'hour')) {
-  // Past
-  console.log("Past");
-  $(this).removeClass("present future").addClass("past");
-} else if (blockTime.isSame(currentTime, 'hour')) {
-  // Present
-  console.log("Present");
-  $(this).removeClass("past future").addClass("present");
-} else {
-  // Future
-  console.log("Future");
-  $(this).removeClass("past present").addClass("future");
-}
+    console.log(`Block Time: ${blockTime.format("HH:mm")}`);
+    console.log(`Current Time: ${currentTime.format("HH:mm")}`);
 
-  console.log(""); // Blank console log for clarity
-});
+    // Display the saved input
+    if (savedInput !== null) {
+      $(this).children(".description").val(savedInput);
+    }
+
+    // Change the color of the time blocks based on the current time
+    if (blockTime.isBefore(currentTime, 'hour')) {
+      // Past
+      console.log("Past");
+      $(this).removeClass("present future").addClass("past");
+    } else if (blockTime.isSame(currentTime, 'hour')) {
+      // Present
+      console.log("Present");
+      $(this).removeClass("past future").addClass("present");
+    } else {
+      // Future
+      console.log("Future");
+      $(this).removeClass("past present").addClass("future");
+    }
+
+    console.log(""); // Blank console log for clarity
+  });
 
   // Event listener for clear button
-  $("#clearBtn").click(function() {
+  $("#clearBtn").click(function () {
     // Ask user to confirm
     var confirmClear = confirm("Are you sure you want to clear your schedule?");
-    if (confirmClear === true) {
+    if (confirmClear) {
       $(".description").val("");
       localStorage.clear();
       alert("Your schedule has been cleared.");
