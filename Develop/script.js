879cb71103bc7077931b5f37d44184f3ff0d10fd
Indexal <148807519+Index-al@users.noreply.github.com>
2023-12-01 16:34:16 -0600

    Added date in header and logic for saving user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,50 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Get the current hour using Day.js
+var currentHour = dayjs().hour();
+
+// Render all elements of the HTML before executing any JS (.ready)
+$( document ).ready ( function () {
+  $(".saveBtn").click(function() {
+    var timeID = $(this).parent().attr('id')
+    var userInput = $(this).siblings(".description").val()
+    console.log("Click!\n" + "timeID = " + timeID);
+    console.log("userInput = " + userInput);
+
+    // Save to local storage
+    localStorage.setItem(timeID, userInput);
+  })
+
+// Load any saved data from LocalStorage and update color of time blocks
+$(".time-block").each(function () {
+  var timeID = $(this).attr('id');
+  var savedInput = localStorage.getItem(timeID);
+  var blockHour = parseInt(timeID.split("-")[1]);
+
+  // Create Day.js objects for block and current time
+  var blockTime = dayjs().hour(blockHour).minute(0);
+  var currentTime = dayjs().hour(currentHour).minute(0);
+
+  console.log("Block Time: " + blockTime.format("HH:mm"));
+  console.log("Current Time: " + currentTime.format("HH:mm"));
+
+  // Display the saved input
+  if (savedInput !== null) {
+    $(this).children(".description").val(savedInput);
+  }
+
+  console.log(""); // Blank console log for clarity
+});
+
+  // Event listener for clear button
+  $("#clearBtn").click(function() {
+    // Ask user to confirm
+    var confirmClear = confirm("Are you sure you want to clear your schedule?");
+    if (confirmClear === true) {
+      $(".description").val("");
+      localStorage.clear();
+      alert("Your schedule has been cleared.");
+    }
+  });
+
+  // Show the current date in the header
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 });
